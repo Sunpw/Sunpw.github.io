4b2f61aabe1230d3d454b593317fd417231ece2b
974436591 <974436591.qq.com>
2016-06-05 21:52:38 +0800

    88

--- src/scripts/view/home.js
+++ src/scripts/view/home.js
@@ -12,7 +12,7 @@ SPA.defineView('home', {
     'beforeShow':function(){
       var vm=this.getVM();
       $.ajax({
-        url:'/yhlife/mock/mock.js',
+        url:'/yhlife/mock/lifelist.json',
         success:function(res){
           vm.lifelist=res.data;
         }
